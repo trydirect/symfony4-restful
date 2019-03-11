91d36e6f69bed94b348c9716f563d90740a56935
Ovruchsky <OvruchskyAlex@ya.ru>
2019-03-11 08:55:46 +0200

    installed rest api

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
---
+++ symfony/src/Controller/Rest/RestController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 3/11/19
+ * Time: 8:36 AM
+ */
+
+namespace App\Controller\Rest;
+
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class RestController extends FOSRestController
+{
+
+
+
+}
