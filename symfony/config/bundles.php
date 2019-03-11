e7257beb75f23981f423c06d5d22e014b2af4a3a
Ovruchsky <OvruchskyAlex@ya.ru>
2019-03-11 16:09:25 +0200

    installed doctrine

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -5,4 +5,7 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
